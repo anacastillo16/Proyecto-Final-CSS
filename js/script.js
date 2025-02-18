bf3ebb0015e291a7caf0a37a6476cc5ea1ce51a4
Ana Castillo Ramírez <anacastilloramirez16@gmail.com>
2025-02-18 18:46:13 +0100

    Añadiendo js al proyecto

---
+++ js/script.js
@@ -0,0 +1,145 @@
+let peliculas = [
+  {
+    titulo: "We Live In Time",
+    descripcion:
+      "Almut y Tobias se conocen en un encuentro inesperado que cambia sus vidas. A través de pasajes de su vida en común se enamoran, construyen un hogar, forman una familia se nos revela una difícil verdad que amenaza con sacudir sus cimientos. A medida que emprenden un camino que los límites del tiempo desafían, los protagonistas aprenderán a apreciar cada momento del inusual camino que ha tomado su historia de amor, que abarca una década.",
+    anio: 2024,
+    imagen: "img/el tiempo que tenemos.avif",
+  },
+  {
+    titulo: "It Ends with Us",
+    descripcion:
+      "Lily es una chica que supera una infancia traumática para embarcarse en una nueva vida. Un encuentro casual con un neurocirujano despierta una chispa entre ambos, pero Lily comienza a ver en él aspectos que le recuerdan a la relación de sus padres.",
+    anio: 2024,
+    imagen: "img/romper el circulo.avif",
+  },
+  {
+    titulo: "Culpa Mía",
+    descripcion:
+      "Noah debe dejar su ciudad, novio y amigos para mudarse a la mansión de William Leister, el flamante y rico marido de su madre Rafaela. Por otro lado, con 17 años, orgullosa e independiente, Noah se resiste a vivir en una mansión rodeada de lujo. Allí conoce a Nick, su nuevo hermanastro, y el choque de sus fuertes personalidades se hace evidente desde el primer momento.",
+    anio: 2023,
+    imagen: "img/culpa mia.avif.crdownload",
+  },
+  {
+    titulo: "Culpa Tuya",
+    descripcion:
+      "A pesar del énfasis de sus padres en separarlos, el amor de Noah y Nick parece indestructible. Sin embargo, el trabajo y la universidad abren sus vidas a nuevas relaciones.",
+    anio: 2024,
+    imagen: "img/culpa tuya.avif",
+  },
+  {
+    titulo: "Harry Potter and the Philosopher's Stone",
+    descripcion:
+      "Harry Potter es un huérfano que vive con sus desagradables tíos, los Dursley, y su repelente primo Dudley. Se acerca su undécimo cumpleaños y tiene pocas esperanzas de recibir algún regalo, ya que nunca nadie se acuerda de él. Sin embargo, pocos días antes de su cumpleaños, una serie de misteriosas cartas dirigidas a él y escritas con una estridente tinta verde rompen la monotonía de su vida: Harry es un mago y sus padres también lo eran.",
+    anio: 2001,
+    imagen: "img/hp piedra.avif",
+  },
+  {
+    titulo: "Harry Potter and the Chamber of Secrets",
+    descripcion:
+      "Harry regresa a su segundo año a Hogwarts, pero descubre que cosas malas ocurren debido a que un sitio llamado la Cámara de los Secretos ha sido abierto por el heredero de Slytherin y hará que los hijos de muggles, los impuros, aparezcan petrificados misteriosamente por un animal monstruoso.",
+    anio: 2002,
+    imagen: "img/hp camara.avif",
+  },
+  {
+    titulo: "Harry Potter and the Prisoner of Azkaban",
+    descripcion:
+      "Harry está deseando que termine el verano para comenzar un nuevo curso en Hogwarts, y abandonar lo antes posible la casa de sus despreciables tíos, los Dursley. Lo que desconoce Harry es que va a tener que abandonar Privet Drive antes de tiempo e inesperadamente después de convertir a su tía Marge en un globo gigante. Un autobús noctámbulo, y encantado por supuesto, le llevará a la taberna El Caldero Chorreante, donde le espera nada menos que Cornelius Fudge, el Ministro de Magia.",
+    anio: 2004,
+    imagen: "img/hp prisionero.avif",
+  },
+  {
+    titulo: "Harry Potter and the Goblet of Fire",
+    descripcion:
+      "En el cuarto año en Hogwarts, Harry se enfrenta al mayor de los desafíos y peligros de la saga. Cuando es elegido bajo misteriosas circunstancias como el competidor que representará a Hogwarts en el Torneo Triwizard, Harry deberá competir contra los mejores jóvenes magos de toda Europa. Pero mientras se prepara, aparecen pruebas que manifiestan que Lord Voldemort ha regresado. Antes de darse cuenta, Harry no solo estará luchando por el campeonato sino también por su vida.",
+    anio: 2005,
+    imagen: "img/hp caliz.avif",
+  },
+  {
+    titulo: "Harry Potter and the Order of the Phoenix",
+    descripcion:
+      "Harry Potter regresa por quinto año a Hogwarts aún sacudido por la tragedia ocurrida en el Torneo de los Tres Magos. Debido a que el Ministro de la Magia niega el regreso de Lord Voldemort, Harry se convierte en el centro de atención de la comunidad mágica. Mientras lucha con sus problemas en el colegio, incluyendo a la nueva profesora Dolores Umbridge, intentará aprender más sobre la misteriosa Orden del Fénix.",
+    anio: 2007,
+    imagen: "img/hp orden.avif",
+  },
+  {
+    titulo: "Harry Potter and the Half-Blood Prince",
+    descripcion:
+      "En medio de los desastres que azotan a Inglaterra, Harry y sus compañeros vuelven a Hogwarts para cursar su sexto año de estudios; y aunque las medidas de seguridad han convertido al colegio en una fortaleza, algunos estudiantes son víctimas de ataques inexplicables. Harry sospecha que Draco Malfoy es el responsable de los mismos y decide averiguar la razón. Mientras tanto, Albus Dumbledore y el protagonista exploran el pasado de Lord Voldemort mediante recuerdos que el director ha recolectado. Con esto, Dumbledore planea preparar al muchacho para el día de la batalla final.",
+    anio: 2009,
+    imagen: "img/hp misterio.avif",
+  },
+  {
+    titulo: "Harry Potter and the Deathly Hallows: Part 1",
+    descripcion:
+      "Una tarea casi imposible cae sobre los hombros de Harry: deberá encontrar y destruir los horrocruxes restantes para dar fin al reinado de Lord Voldemort. En el episodio final de la saga, el hechicero de 17 años parte junto con sus amigos Hermione Granger y Ron Weasley en un peligroso viaje por Inglaterra para encontrar los objetos que contienen los fragmentos del alma del Señor Tenebroso, los cuales garantizan su longevidad. Pero el camino no será fácil pues el lado oscuro adquiere más poder con cada minuto que pasa y las lealtades serán puestas a prueba. Harry deberá usar todos los conocimientos que gracias a Dumbledore ha adquirido sobre su enemigo para poder encontrar la forma de sobrevivir a esta última aventura.",
+    anio: 2010,
+    imagen: "img/hp reliquias 1.avif",
+  },
+  {
+    titulo: "Harry Potter and the Deathly Hallows: Part 2",
+    descripcion:
+      "La segunda parte de la batalla final entre las fuerzas del bien y el mal. El juego nunca ha sido tan peligroso y nadie está a salvo. Se acerca el momento de la confrontación final entre Harry Potter y Lord Voldemort. Todo termina aquí…",
+    anio: 2011,
+    imagen: "img/hp reliquias 2.avif",
+  },
+];
+
+let peliculasContainer = document.getElementById("peliculas");
+
+formulario.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let peliABuscar = document.getElementById("nombrePeli").value.toLowerCase();
+
+  peliculasContainer.innerHTML = "";
+
+  let peliculasEncontradas = peliculas.filter((pelicula) =>
+    pelicula.titulo.toLowerCase().includes(peliABuscar)
+  );
+
+  if (peliculasEncontradas.length > 0) {
+    peliculasEncontradas.forEach((pelicula) => {
+      let article = document.createElement("article");
+      article.classList.add("pelicula");
+
+      let titulo = document.createElement("h3");
+      titulo.textContent = pelicula.titulo;
+
+      let contenido = document.createElement("div");
+      contenido.classList.add("contenido");
+
+      let imagen = document.createElement("img");
+      imagen.src = pelicula.imagen;
+      imagen.alt = pelicula.titulo;
+
+      let texto = document.createElement("div");
+      texto.classList.add("texto");
+
+      let descripcion = document.createElement("p");
+      descripcion.textContent = pelicula.descripcion;
+
+      let año = document.createElement("p");
+      año.classList.add("year");
+      año.textContent = "Año de estreno: " + pelicula.anio;
+
+      let boton = document.createElement("a");
+      boton.classList.add("boton");
+      boton.href = "#";
+      boton.textContent = "Ver más";
+
+      texto.appendChild(descripcion);
+      texto.appendChild(año);
+      contenido.appendChild(imagen);
+      contenido.appendChild(texto);
+      article.appendChild(titulo);
+      article.appendChild(contenido);
+      article.appendChild(boton);
+
+      peliculasContainer.appendChild(article);
+    });
+  } else {
+    let mensajeError = document.createElement("p");
+    mensajeError.textContent = "No se encontraron películas con ese título.";
+    peliculasContainer.appendChild(mensajeError);
+  }
+});
